981574043a58a19c8bd033b3f87d327f2ddbfeb4
Arian Gallardo <gallardo.a@pucp.pe>
2020-07-19 18:20:09 -0500

    :bug: Fix import bug on user mode

---
+++ models/utils.py
@@ -0,0 +1,12 @@
+from app import db 
+
+class AddUpdateDelete():
+	def add(self, resource):
+		db.session.add(resource)
+	
+	def update(self):
+		pass
+	
+	def delete(self, resource):
+		db.session.delete(resource)
+		return db.session.commit()
